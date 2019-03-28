4f621670760225569449c6cf464832643b2f4782
ani1203 <mehta.anil1507@gmail.com>
2019-03-28 19:16:51 +0530

    making some changes in dependencies.sh

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-chown ubuntu:ubuntu /home/ubuntu
 virtualenv /home/ubuntu/venv
-chown ubuntu:ubuntu /home/ubuntu/venv
-chown ubuntu:ubuntu /home/ubuntu/venv/*
 source /home/ubuntu/venv/bin/activate
 pip3 install -r /home/ubuntu/cds-demo/requirements/base.txt
 pip3 install gunicorn
