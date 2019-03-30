ff68befb6c53c79cca241e101030eeb79c00f397
ani1203 <46773492+ani1203@users.noreply.github.com>
2019-03-30 14:55:57 +0530

    Update dependencies.sh

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,6 +2,6 @@
 sudo apt-get install python3-pip python3-dev nginx git -y
 sudo apt-get install virtualenv -y
 virtualenv --python=python3 /home/ubuntu/venv
-chown ubuntu:ubuntu /home/ubuntu/cds-demo
+sudo chown ubuntu:ubuntu /home/ubuntu/cds-demo
 
 
