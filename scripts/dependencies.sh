303d5404856dd0b7fc66b23eacd55830cda5fad3
ani1203 <46773492+ani1203@users.noreply.github.com>
2019-03-30 19:22:06 +0530

    Update dependencies.sh

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -5,6 +5,8 @@ virtualenv --python=python3 /home/ubuntu/venv
 sudo chown ubuntu:ubuntu /home/ubuntu/cds-demo
 source /home/ubuntu/venv/bin/activate
 pip3 install -r /home/ubuntu/cds-demo/requirements/base.txt
+pip3 install django bcrypt django-extensions
+pip3 install gunicorn 
 
 
 
