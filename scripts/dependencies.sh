5dc641469e7f097e74ba77bba5d6636f02ec5cfd
ani1203 <46773492+ani1203@users.noreply.github.com>
2019-03-31 15:54:08 +0530

    Update dependencies.sh

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git clone https://github.com/ani1203/learnDjango.git
 sudo apt-get install python3-pip python3-dev nginx git -y
+git clone https://github.com/ani1203/learnDjango.git 
 sudo apt-get install virtualenv -y
 virtualenv --python=python3 /home/ubuntu/venv
 sudo chown ubuntu:ubuntu /home/ubuntu/cds-demo
