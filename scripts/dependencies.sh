f03a7868734f384203d9a42527d435498e073800
ani1203 <46773492+ani1203@users.noreply.github.com>
2019-03-31 12:10:10 +0530

    Update dependencies.sh

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -5,11 +5,9 @@ virtualenv --python=python3 /home/ubuntu/venv
 sudo chown ubuntu:ubuntu /home/ubuntu/cds-demo
 source /home/ubuntu/venv/bin/activate
 pip3 install -r /home/ubuntu/cds-demo/requirements/base.txt
-pip3 install django bcrypt django-extensions
-pip3 install gunicorn 
-cp -r  /home/ubuntu/cds-demo/required-files  /etc/systemd/system
-systemctl daemon-reload
-systemctl start gunicorn
-systemctl enable gunicorn
-
+pip3 install django bcrypt django-extensions 
+sudo cp /home/ubuntu/cds-demo/required-files/gunicorn.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
 
